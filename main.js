a3cf9846401a8b3a4fc94479dbf5b121710bb935
Ahmed Badry <ra6514201@gmail.com>
2024-07-27 07:25:49 +0300

    create html , css and js files and end the header

---
+++ main.js
@@ -0,0 +1,20 @@
+const btnMenu = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+btnMenu.addEventListener('click', function(event) {
+    event.stopPropagation(); // Prevent the click event from bubbling up to the document
+    menu.classList.toggle('active');
+
+    if (this.style.transform === 'scale(1.1) rotate(90deg)') {
+        this.style.transform = 'scale(1) rotate(0deg)';
+    } else {
+        this.style.transform = 'scale(1.1) rotate(90deg)';
+    }
+});
+
+document.addEventListener('click', function(event) {
+    if (!menu.contains(event.target) && event.target !== btnMenu) {
+        menu.classList.remove('active');
+        btnMenu.style.transform = 'scale(1) rotate(0deg)';
+    }
+});
