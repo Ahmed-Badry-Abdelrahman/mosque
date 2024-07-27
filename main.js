34aa0ad7dc9bd6de61b44fb1c95d8ab3f10bb55d
Ahmed Badry <ra6514201@gmail.com>
2024-07-27 09:40:35 +0300

    "Added zoomIn animation to menu, updated CSS styles for menu and navigation, and modified JavaScript to toggle zoomIn class on menu button click."

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menu = document.getElementById('menu');
 btnMenu.addEventListener('click', function(event) {
     event.stopPropagation(); // Prevent the click event from bubbling up to the document
     menu.classList.toggle('active');
+    menu.classList.toggle('zoomIn');
 
     if (this.style.transform === 'scale(1.1) rotate(90deg)') {
         this.style.transform = 'scale(1) rotate(0deg)';
@@ -15,6 +16,7 @@ btnMenu.addEventListener('click', function(event) {
 document.addEventListener('click', function(event) {
     if (!menu.contains(event.target) && event.target !== btnMenu) {
         menu.classList.remove('active');
+        menu.classList.remove('zoomIn');
         btnMenu.style.transform = 'scale(1) rotate(0deg)';
     }
 });
